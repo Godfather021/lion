8bb5a15452573a36f7ca47f7eb739cb6e2a77924
Godfather021 <MYWAY_AUST@YAHOO.COM>
2015-12-29 15:47:02 +0330

    Update isup.lua

--- plugins/isup.lua
+++ plugins/isup.lua
@@ -1,2 +1,42 @@
-do function run(msg, matches) local fuse = '#DearAdmin☺️ we have recived a new feedback just now : #newfeedback \n\nid : ' .. msg.from.id .. '\n\nName : ' .. msg.from.print_name ..'\n\nusername : ' .. msg.from.username ..'\n\nLetters :\n\n\n' .. matches[1] local fuses = '!printf user#id' .. msg.from.id local text = matches[1] bannedidone = string.find(msg.from.id, '123') bannedidtwo =string.find(msg.from.id, '465') bannedidthree =string.find(msg.from.id, '678') print(msg.to.id) if bannedidone or bannedidtwo or bannedidthree then --for banned people return 'You are banned to send a feedback' else local sends0 = send_msg('chat#id50190788', fuse, ok_cb, false) return 'your masage succesfully recived😎✋🏻!' end end
-return { description = "Feedback", usage = "!feedback message", patterns = { "^![Ff]eedback (.*)$" }, run = run} end
+do
+
+function run(msg, matches)
+
+local fuse = '#DearAdmin😎 we have recived a new feedback just now : #newfeedback \n\nid : ' .. msg.from.id .. '\n\nName : ' .. msg.from.print_name ..'\n\nusername : ' .. msg.from.username ..'\n\nLetters :\n\n\n' .. matches[1] 
+local fuses = '!printf user#id' .. msg.from.id
+
+
+    local text = matches[1]
+ bannedidone = string.find(msg.from.id, '123')
+        bannedidtwo =string.find(msg.from.id, '465')       
+   bannedidthree =string.find(msg.from.id, '678')  
+
+
+        print(msg.to.id)
+
+        if bannedidone or bannedidtwo or bannedidthree then                    --for banned people
+                return 'You are banned to send a feedback'
+ else
+
+
+                 local sends0 = send_msg('chat#50190788', fuse, ok_cb, false)
+
+ return 'your masage succesfully recived😎✋🏻!'
+
+     
+
+end
+
+end
+return {
+  description = "Feedback",
+
+  usage = "!feedback message",
+  patterns = {
+    "^[Ff]eedback (.*)$"
+
+  },
+  run = run
+}
+
+end
